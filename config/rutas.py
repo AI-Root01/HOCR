e1af0ba80c862f877fc321514615dfba14936080
Frank Prime <frank_primero@protonmail.com>
2026-03-16 20:14:53 -0600

    Add files via upload

---
+++ config/rutas.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parents[1] 
+
+HOCR_DIR       = ROOT_DIR / "archivos_hocr"
+PDF_DIR        = ROOT_DIR / "archivos_pdf"
+RESULTADOS_DIR = ROOT_DIR / "resultados"
